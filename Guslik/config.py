59645d1a8b6db934a27d07e791893d0bf2bda11d
ArtemZaZ <temka.911@mail.ru>
2018-09-27 19:16:37 +0300

    Исправил косяки

--- Guslik/config.py
+++ Guslik/config.py
@@ -10,16 +10,16 @@ from RPiPWM import *
 IP = '173.1.0.78'  # IP адрес куда отправляем видео
 RPCServerPort = 8000  # порт RPC сервера
 
-chanSrvFL = 1  # канал для передней левой сервы
-chanSvrFR = 2  # канал для передней правой сервы
-chanSrvBL = 3  # канал для задней левой сервы
-chanSrvBR = 4  # канал для задней правой сервы
+chanSrvFL = 9  # канал для передней левой сервы
+chanSvrFR = 8  # канал для передней правой сервы
+chanSrvBL = 11  # канал для задней левой сервы
+chanSrvBR = 10  # канал для задней правой сервы
 chanSvrCAM = 5  # канал для сервы с камерой
 servoResolutionDeg = -90, 90    # разрешение с центром в нуле
-servoResolutionMsk = 800, 2400
+servoResolutionMcs = 800, 2400
 
-chanRevMotorLB = 12  # каналы моторов, индексы аналогичны сервам
-chanRevMotorRB = 13
+chanRevMotorLB = 14  # каналы моторов, индексы аналогичны сервам
+chanRevMotorRB = 15
 
 SvrFL = Servo270(chanSrvFL)  # передняя левая
 SvrFR = Servo270(chanSvrFR)  # передняя правая
@@ -32,12 +32,12 @@ MotorRB = ReverseMotor(chanRevMotorRB)
 
 def servoScale(value):  # рескейлим серву, как нам нужно
     degRange = (servoResolutionDeg[1] - servoResolutionDeg[0])
-    mskRange = (servoResolutionMsk[1] - servoResolutionMsk[0])
-    result = ((value - servoResolutionDeg[0])/degRange) * mskRange + servoResolutionMsk[0]
-    if result > servoResolutionMsk[1]:
-        return servoResolutionMsk[1]
-    elif result < servoResolutionMsk[0]:
-        return servoResolutionMsk[0]
+    mskRange = (servoResolutionMcs[1] - servoResolutionMcs[0])
+    result = ((value - servoResolutionDeg[0])/degRange) * mskRange + servoResolutionMcs[0]
+    if result > servoResolutionMcs[1]:
+        return servoResolutionMcs[1]
+    elif result < servoResolutionMcs[0]:
+        return servoResolutionMcs[0]
     else:
         return int(result)
 
@@ -49,12 +49,13 @@ def rotate(speed):
 
 def turnForward(scale):
     """ поворот передней части робота """
-    SvrFL.SetValue(servoScale(90 * scale))
-    SvrFR.SetValue(servoScale(90 * scale))
+    SvrFL.SetMcs(servoScale(90 * scale))
+    SvrFR.SetMcs(servoScale(90 * scale))
     return True
+
 
 def move(speed):
     """ движение вперед/назад """
-    MotorLB.SetValue(speed)
-    MotorRB.SetValue(-speed)
+    MotorLB.SetValue(-speed)
+    MotorRB.SetValue(speed)
     return True
